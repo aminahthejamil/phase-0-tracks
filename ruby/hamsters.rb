b6e0ddd351f1793c5562ed6e0dcc5084b05f8557
Aminah Burch <a.j.burch11@gmail.com>
2017-07-19 11:31:12 -0500

    Add hamster sanctuary questionnaire

---
+++ ruby/hamsters.rb
@@ -0,0 +1,18 @@
+#Hamster Sanctuary
+
+hamster_name = gets.chomp
+volume = gets.chomp
+fur_color = gets.chomp
+adoptable = gets.chomp
+estimated_age = nil
+
+puts "What's the hamster's name?"
+     hamster_name
+puts "What's your sleep sensitivity from 1-10?"
+     volume
+puts "What's the color of the hamster's fur?"
+     fur_color
+puts "Would you recommend this hamster for adoption, y or n?"
+     adoptable
+puts "How old would you say this hamster is?"
+     estimated_age          
