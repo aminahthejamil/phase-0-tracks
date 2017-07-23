3e0ae4bd0e5c5110c4f6e397bc6c9173cf96eacb
Aminah Burch <a.j.burch11@gmail.com>
2017-07-23 16:35:41 -0500

    Add questions for vampire doc

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What is your age?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Should we order you some garlic bread? (y/n)"
+garlic_bread = gets.chomp
+puts "Would you like to be enrolled in the company's life insurance program? (y/n)"
+life_insurance = gets.chomp
+
+if 
