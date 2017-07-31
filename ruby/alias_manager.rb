ba6abe73e75f2c914fee771c3c3295ece9a11eb3
Aminah Jamil <am.jam@aminahjamil.com>
2017-07-31 10:01:55 -0500

    Update loop to fix database entries

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,6 +11,19 @@ Switch the first and last name
 
 =end
 
+#Pseudocode
+=begin
+Switch the first and last name
+1. Separate the names into 2 array objects
+2. Swap the names
+3. Downcase the names
+4. Switch out the vowels to the next vowels
+5. Switch out the consonants to the next consonants
+6. Capitalize the names
+7. Out put the new alias name
+
+=end
+
 def down_case(str)
   str.downcase
 end
@@ -40,44 +53,27 @@ def capitalize(str)
   str.capitalize
 end
 
+alias_database = []
+puts "Welcome to the Alias Creator! Type 'quit' to finish"
+loop do 
 
-
-def alias_machine
-  puts "Welcome to the Alias Creator! Type 'quit' to finish"
   puts " "
   puts "What's your first name?"
   first_name = gets.chomp!
-
+  break if first_name == 'quit'
   puts "What's your last name?"
   last_name = gets.chomp
 
   real_name = first_name + " " + last_name
   new_alias = capitalize(last_name) + " " + capitalize(first_name)
-
-
+  
+  alias_database << real_name
+  alias_database << new_alias
   puts "Your new Alias is: "
   puts new_alias
-
-  alias_database = {
-    real_name: "",
-    alias_name: ""
-  }
-
-  alias_database[:real_name] = first_name + " " +  last_name
-
-  alias_database[:alias_name] = new_alias
-
-
-  puts "Would you like to continue adding names? (y/n)"
-  user_input = gets.chomp
-  if user_input == "y"
-    puts alias_machine
-  else
-    puts alias_database
-  end
-
-
-
+  
 end
 
-puts alias_machine
+puts alias_database
+
+
