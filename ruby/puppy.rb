79296b1c7e8dcd671a4ba08aa9986e47c98216bd
Aminah Burch <a.j.burch11@gmail.com>
2017-08-01 12:11:45 -0500

    Add initial file contents

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+SPECIES ------------------------
+Eyelashus Adorableus
+
+CHARACTERISTICS ----------------
+Eyelash length: varies
+Friendly: yes
+Eye count: 8
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Hug
+Scamper
