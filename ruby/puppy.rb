1d2b1e90d2c636723b4111f750afeb0d39b179bf
Aminah Jamil <am.jam@aminahjamil.com>
2017-08-02 15:24:49 -0500

    Add comments to the puppy description lines

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,18 +1,18 @@
-SPECIES ------------------------
-Puppy
+# SPECIES
+# Puppy
 
-CHARACTERISTICS ----------------
-Ears: varies
-Friendly: yes
-Fur type: varies
-Name: varies
-Color: varies
-Leg count: 4 (sometimes less)
+# CHARACTERISTICS
+# Ears: varies
+# Friendly: yes
+# Fur type: varies
+# Name: varies
+# Color: varies
+# Leg count: 4
 
-BEHAVIOR -----------------------
-Jump
-Lick
-Bite
-Run
-Snuggle
-Dig
+# BEHAVIOR 
+# Jump
+# Lick
+# Bite
+# Run
+# Snuggle
+# Dig
