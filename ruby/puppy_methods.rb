82846d227b232e30da6732dced333db6a154f61e
Aminah Burch <a.j.burch11@gmail.com>
2017-08-03 12:16:39 -0500

    Add methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,21 @@ class Puppy
 
 end
 
+  def speak(num)
+    puts "Woof!"
+  end
+
+  def roll_over(action)
+    action = "*rolls over*"
+    puts "#{{action}}"
+  end
+
+  def dog_years(dog_years)
+    7 * dog_years = human_years
+    human_years
+  end
+
+  def lick(action)
+    action = "*lick*"
+  end
+# Driver Code
