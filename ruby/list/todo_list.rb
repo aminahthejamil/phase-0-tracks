7fac437263e697085d989ab040ba83291b3a8c0c
Aminah Burch <a.j.burch11@gmail.com>
2017-08-05 09:58:10 -0500

    Restructure methods in the todolist

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,13 +1,40 @@
 class TodoList
-  def initialize(get_items)
-    @get_items = get_items
+	def initialize(chore1, chore2)
+		@List = [chore1, chore2]
 	end
-  get_items = ["do the dishes", "mow the lawn"]
+
   def get_items
-    get_items = ["do the dishes", "mow the lawn"]
+    return @List
   end
 
-  def add_item(chore)
-    get_items <<
+  def add_item(target)
+  	arrayLen = @List.length
+  	@List[arrayLen] = target
+  	return @List
   end
+
+  def delete_item(target)
+  	ctr = 0
+  	ctr2 = 0
+  	newArray = []
+  	arrayLen = @List.length
+  	while ctr < arrayLen
+  		if target != @List[ctr]
+  			newArray[ctr2] = @List[ctr]
+  			ctr2 += 1
+  		end
+  		ctr += 1
+  	end
+  	@List = newArray
+  	return @List
+  end
+
+  def get_item(index)
+  	newArray = []
+  	newArray << @List[index]
+  	return newArray
+  end
+
+
+
 end
