83b152a32e26d58eef4f78819675cffa7db2f122
Aminah Burch <a.j.burch11@gmail.com>
2017-08-04 23:01:51 -0500

    Add require relative line

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,23 +1,23 @@
-require_relative 'TodoList'
+require_relative 'todo_list'
 
 describe TodoList do
-  let(:todolist) { TodoList.new(["do the dishes", "mow the lawn"]) }
+  let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
   it "stores the list items given on initialization" do
-    expect(todolist.get_items).to eq ["do the dishes", "mow the lawn"]
+    expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
   end
 
   it "adds an item to the list" do
     list.add_item("mop")
-    expect(todolist.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
+    expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
   end
 
   it "deletes an item" do
     list.delete_item("do the dishes")
-    expect(todolist.get_items).to eq ["mow the lawn"]
+    expect(list.get_items).to eq ["mow the lawn"]
   end
 
   it "retrieves an item by index" do
-    expect(todolist.get_item(0)).to eq "do the dishes"
+    expect(list.get_item(0)).to eq "do the dishes"
   end
 end
