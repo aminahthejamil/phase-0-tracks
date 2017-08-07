db70d4d5cf0907ab67c534266b69b073d7eecbcc
Aminah Burch <a.j.burch11@gmail.com>
2017-08-06 23:52:54 -0500

    Add some instance methods, and test with driver code

--- ruby/guessinggame.rb
+++ ruby/guessinggame.rb
@@ -1,13 +1,17 @@
 class GuessingGame
   attr_reader
-  attr_accessor
+  attr_accessor :word_array
 
   def initialize
     @guess_count = 0
-    @game_over = false
+    @is_over = false
+    @word_array = Array.new
   end
 
-  def
+  def get_letter
+    @guess_count += 1
+    if
+  end
 
 end
 
@@ -15,4 +19,18 @@ end
 
 puts "Welcome to the word guessing game, where one player enters a word and the other one guesses it!"
 
-game = GuessingGame.new    
+game = GuessingGame.new
+
+puts "Player 1, please enter a word: "
+p1_input = gets.chomp
+
+
+word_array << p1_input.split('')
+
+=begin
+while !game.is_over
+  puts "Enter a letter:"
+  guess = gets.chomp
+  if !game.get_letter(guess - 1)
+end
+=end
