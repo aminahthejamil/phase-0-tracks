2d4ab997507d79aa011d5cce9fd4dd0f56df5626
Aminah Burch <a.j.burch11@gmail.com>
2017-08-07 10:37:25 -0500

    Add driver code

--- ruby/guessinggame.rb
+++ ruby/guessinggame.rb
@@ -1,31 +1,85 @@
-class GuessingGame
-  attr_reader
-  attr_accessor :word_array
+class WordGuess
+  attr_reader :the_word
+  attr_accessor :the_word, :placeholder
 
-  def initialize
-    @guess_count = 0
+  def initialize(the_word)
     @is_over = false
-    @word_array = Array.new
+    @the_word = the_word.downcase
+    @placeholder = "_" * the_word.length
+    puts @placeholder
   end
 
-  def get_letter
-    @guess_count += 1
-    if
+  def get_letters(guessed_letter)
+    @the_word.split('').each_index do |good_letter, index|
+      if guessed_letter == good_letter
+        @placeholder[index] = guessed_letter
+        puts "Yes! #{quessed_letter} is in the word!"
+      end
+      p @placeholder
+      if !@the_word.include?(guessed_letter)
+        puts "#{guessed_letter} is not in this word, please try again"
+      end
+    end
   end
-
 end
-
 # User Interface
 
 puts "Welcome to the word guessing game, where one player enters a word and the other one guesses it!"
 
-game = GuessingGame.new
-
-puts "Player 1, please enter a word: "
-p1_input = gets.chomp
 
 
-word_array << p1_input.split('')
+puts "Player 1, please enter a word:"
+p1_word = gets.chomp
+
+game = WordGuess.new(p1_word)
+game.the_word = p1_word
+
+letters_guessed = []
+guesses = 0
+
+until guesses == p1_word.length * 2
+
+  puts "Player 2, go on and guess a letter!"
+  p2_letter = gets.chomp
+  break if p2_letter == "quit"
+
+  if letters_guessed.include? p2_letter
+    puts "Sorry, you already guessed that letter. Guess again!"
+    p2_letter = gets.chomp
+    break if p2_letter == "quit"
+  end
+
+  letters_guessed << p2_letter
+
+  game.get_letters(p2_letter)
+
+  guesses += 1
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+player_one_word.each do |letters|
+  letter.length
+  puts
+
+=end
+
 
 =begin
 while !game.is_over
