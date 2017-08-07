5ca09dd47d74f327c20d163c70295de6134c09fc
Aminah Burch <a.j.burch11@gmail.com>
2017-08-06 23:28:38 -0500

    Add initial class syntax to guessing game

---
+++ ruby/guessinggame.rb
@@ -0,0 +1,18 @@
+class GuessingGame
+  attr_reader
+  attr_accessor
+
+  def initialize
+    @guess_count = 0
+    @game_over = false
+  end
+
+  def
+
+end
+
+# User Interface
+
+puts "Welcome to the word guessing game, where one player enters a word and the other one guesses it!"
+
+game = GuessingGame.new    
