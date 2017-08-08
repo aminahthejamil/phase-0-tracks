e451c7d0fb2a7afbfb7f19ecfa5f7e940bee6c40
Aminah Burch <a.j.burch11@gmail.com>
2017-08-07 20:30:25 -0500

    Add completed code from repl testing

--- ruby/guessinggame.rb
+++ ruby/guessinggame.rb
@@ -1,61 +1,73 @@
-class WordGuess
-  attr_reader :the_word
-  attr_accessor :the_word, :placeholder
+class GuessingWords
+  attr_reader :guesses
+  attr_accessor :is_over, :placeholder, :the_word
 
   def initialize(the_word)
     @is_over = false
+    @guess_arr = []
+    @guesses = the_word.length * 2
     @the_word = the_word
-    @placeholder = "_" * the_word.length
+    @placeholder = Array.new(the_word.length, "_")
+    @ctr = 0
   end
+#Code to get the letters from Player 2 and check them against Player 1's word
+  def get_letters(letter)
+    @guesses += 1
+    if the_word.include?(letter)
+      puts "The word contained #{letter}!"
+      letter_replacement(letter)
+    else
+      puts "Oh, no...#{letter} isn't in the word. :-("
+    end
+    if !@guess_arr.include?(letter)
+      @ctr += 1
+    end
 
-  def get_letters(guessed_letter)
-    @the_word.each_index do |good_letter, index|
-      if guessed_letter == good_letter
-        @placeholder[] = guessed_letter
-        puts "Yes! #{guessed_letter} is in the word!"
-      end
-      puts @placeholder
-      if !@the_word.include?(guessed_letter)
-        puts "#{guessed_letter} is not in this word, please try again"
+    @guess_arr << letter
+
+    if @the_word == @placeholder.join() || @ctr == @guesses
+      game_is_over
+      @is_over = true
+    end
+  end
+#Code to replace the placeholder value "_" with the correct letter if it is guessed by Player 2
+  def letter_replacement(letter)
+    @the_word.split('').each_with_index do |e, i|
+      if e == letter
+        @placeholder[i] = letter
       end
     end
   end
-
-  def game_over
-    if @the_word == @placeholder
-      puts "Congratulations Player 2, you have won the game!"
+#Code to show how far Player 2 has come in their guesses
+  def progression
+    puts "Progress: #{@placeholder.join('')}"
+  end
+  #Code for winning/losing game
+  def game_is_over
+    if @the_word == @placeholder.join()
+      puts "Congratulations! You've won!"
     else
-      puts "Sorry, Player 2, you did not win this time around."
+      puts "Nanner nanner boo boo, you lose. But it's okay, because you're still a winner in my eyes."
     end
   end
 end
-# User Interface
 
-puts "Welcome to the word guessing game, where one player enters a word and the other one guesses it!"
+#User Interface
 
-
-
+#Greeting and Player 1 word input, splits letters of word into individual elements of an array
+puts "Welcome to the Word Guessing game, made just for you!"
 puts "Player 1, please enter a word:"
-p1_word = gets.chomp.split('')
+the_word = gets.chomp
 
-game = WordGuess.new(p1_word)
-game.the_word = p1_word
+game = GuessingWords.new(the_word)
+system "clear"
 
-letters_guessed = []
-guesses = 0
+#game.dashes = ("-" * game.p1_word.length).split('')
 
-puts "Player 2, go on and guess a letter!"
-until guesses == p1_word.length * 2
-  p2_letter = gets.chomp.downcase
-  if letters_guessed.include? p2_letter
-    puts "You already guessed that letter. Guess again!"
-    p2_letter = gets.chomp.downcase
-    break if p2_letter == "quit"
-  end
-
-  letters_guessed << p2_letter
-
+while !game.is_over
+  puts "Player 2, choose a letter (enter 'quit' if you'd like to end the game early):"
+  p2_letter = gets.chomp
+  break if p2_letter == 'quit'
   game.get_letters(p2_letter)
-
-  guesses += 1
+  game.progression
 end
