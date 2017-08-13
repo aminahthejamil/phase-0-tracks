7a5541acfcf38253f427f4c36ce256cc05a6c252
Aminah Burch <a.j.burch11@gmail.com>
2017-08-13 13:30:35 -0500

    Finish writing pseudocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+/*
+1. Get string and set variable equal to it
+2. Reverse the characters
+3. Take the last letter of the original word and add it to the first letter
+position of the new word by looping.
+4. Output the new, reversed word
+*/
