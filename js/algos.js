b9e1cafd04280fa53896cf866f422b421ad55bc8
Aminah Burch <a.j.burch11@gmail.com>
2017-08-13 18:28:05 -0500

    Add pseudocode for release 0

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Release 0
+
+/* 1. Set a variable equal to an array full of words.
+   2. Loop through the array
+   3. Compare each array to the last
+   4. Return the element with the longest length
+
+*/
