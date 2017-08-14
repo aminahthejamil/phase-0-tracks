cbf5baa309377fa930be768b2a5d0120a78bbeb8
Aminah Burch <a.j.burch11@gmail.com>
2017-08-13 20:39:14 -0500

    Add code for longest word release 0

--- js/algos.js
+++ js/algos.js
@@ -4,5 +4,24 @@
    2. Loop through the array
    3. Compare each array to the last
    4. Return the element with the longest length
+   5. Need two loops
+    - one to loop through the array
+    - one to loop through the string characters
 
 */
+
+var arr = ["first item", "second item", "third item is long"];
+var strLength = 0;
+var longestWord;
+
+function theLongestWord(array) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > strLength) {
+      var longestWord = arr.length;
+      longestWord = arr[i];
+    }
+  }
+   return longestWord;
+}
+
+console.log(theLongestWord(arr));
