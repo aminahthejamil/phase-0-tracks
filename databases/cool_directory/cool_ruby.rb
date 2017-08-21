5fe6d2acda096963e110c894f72c8a2748b4fbce
Aminah Burch <a.j.burch11@gmail.com>
2017-08-21 15:04:02 -0500

    Add test values to table

--- databases/cool_directory/cool_ruby.rb
+++ databases/cool_directory/cool_ruby.rb
@@ -10,7 +10,7 @@ db = SQLite3::Database.new( "cool_database.db" )
 
 # Fancy string delimiters
 create_table_cmd = <<-SQL
-  CREATE TABLE kittens(
+  CREATE TABLE IF NOT EXISTS kittens(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     age INT
@@ -19,6 +19,9 @@ SQL
 
 # Create a TABLE
 db.execute(create_table_cmd)
+
+# add a test kittens
+db.execute("INSERT INTO kittens (name, age) VALUES ("Bomas", 5)")
 
 #class Something_Cool
 #end
