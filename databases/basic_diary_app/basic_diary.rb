1a76db1bf7daa269f5f44ded3d881ac5d2a1e72f
Aminah Burch <a.j.burch11@gmail.com>
2017-08-21 16:18:32 -0500

    Add table to database via ruby

--- databases/basic_diary_app/basic_diary.rb
+++ databases/basic_diary_app/basic_diary.rb
@@ -6,8 +6,36 @@
 # - Add title of the entry (if no title entered (if nil) then write 'Untitled')
 # - Add body of text
 # - Give them an array of overall moods to choose from for the day
+  # - Moods will have a different table
 
-# - Extra abilities for the user:
+# - Abilities for the user:
   # - New entry
   # - Edit old entry (based on [date, title, search for words])
   # - Delete entry
+
+# require gems
+  require 'sqlite3'
+  require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("basic_diary.db")
+db.results_as_hash = true
+
+# learn about fancy string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS diary(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    entry_date INT,
+    body VARCHAR(255),
+    mood INT
+  )
+SQL
+
+# create a diary table (if it's not there already)
+
+db.execute(create_table_cmd)
+
+# add a test entry
+
+db.execute("INSERT INTO diary (title, entry_date, body, mood) VALUES ('A New Day', 01012017,  'Dear Diary, today was an exquisite day. I hope to continue our relationship into the future.', 2)")
