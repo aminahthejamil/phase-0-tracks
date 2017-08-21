2c5839c3b07557472c5c4274ddecab7e100d0cb4
Aminah Burch <a.j.burch11@gmail.com>
2017-08-21 14:56:40 -0500

    Add test table through db.execute

--- databases/cool_directory/cool_ruby.rb
+++ databases/cool_directory/cool_ruby.rb
@@ -8,5 +8,17 @@ require 'faker'
 
 db = SQLite3::Database.new( "cool_database.db" )
 
+# Fancy string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+)
+SQL
+
+# Create a TABLE
+db.execute(create_table_cmd)
+
 #class Something_Cool
 #end
